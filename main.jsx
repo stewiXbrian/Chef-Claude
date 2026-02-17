8a9e66ca1ac53f80b965fccd7bb00f7f5dbb1ea8
ayoub <115874830+stewiXbrian@users.noreply.github.com>
2026-02-17 14:50:49 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import App from './my-projects/assembly-game/public/App.jsx'
-  //import App from './my-projects/meme-gen/App.jsx'
-  //import App from './my-projects/chef-claud/App.jsx'
     import App from './src/App.jsx'
 
 
